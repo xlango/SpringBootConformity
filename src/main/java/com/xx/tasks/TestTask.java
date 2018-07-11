3d7fee13181083b83b345ed00e7cac93fdf8fc63
xyl <xyl@DESKTOP-88O51I7>
2018-07-11 21:03:01 +0800

    整合定时任务task

--- src/main/java/com/xx/tasks/TestTask.java
+++ src/main/java/com/xx/tasks/TestTask.java
@@ -14,6 +14,7 @@ public class TestTask {
 	// 定义每过3秒执行任务
    // @Scheduled(fixedRate = 3000)
 	@Scheduled(cron = "4-40 * * * * ?") //在线cron表达式：http://cron.qqe2.com/   注：springboot只支持六位，不支持到年
+	//123
     public void reportCurrentTime() {
         System.out.println("现在时间：" + dateFormat.format(new Date()));
     }
